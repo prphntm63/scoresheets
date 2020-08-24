1f85bb00f61ef7413aba3d6d3284d069f367f6ff
Matt Westwick <matt.westwick@gmail.com>
2020-08-24 01:35:04 -0500

    updated db migrations and models

---
+++ models/competition.ts
@@ -0,0 +1,12 @@
+export interface Competition {
+    'id':string
+    'ctime':string
+    'mtime':string
+    'name':string
+    'description':string
+    'email':string
+    'photo_url':string
+    'entry_deadline':string
+    'award_ceremony':string
+    'scoresheet_format':string
+}
